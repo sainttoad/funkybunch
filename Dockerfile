199bc7012d7ff1894f6d64ea5f978e971680c662
sainttoad <github@sainttoad.com>
2019-04-09 08:50:35 -0700

    funky!

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ from python:3
 ADD app/requirements.txt /tmp/requirements.txt
 RUN pip3 install -r /tmp/requirements.txt
 
+ADD secrets/secrets /secrets
+
 ADD app /funkybunch
 WORKDIR /funkybunch
 
--- app/funkybunch.py
+++ app/funkybunch.py
@@ -1,12 +1,14 @@
 #!/usr/bin/env python3
 
 import markovify
+import twitter
 import csv
 import re
 import os
 
 PATH_TO_MODEL   = '/tweets/model.json'
 PATH_TO_TWEETS  = '/tweets/tweets.csv'
+PATH_TO_SECRETS = '/secrets'
 
 def make_model():
     with open(PATH_TO_TWEETS) as f:
@@ -25,6 +27,21 @@ def make_model():
         with open(PATH_TO_MODEL, 'wb') as f:
             f.write(model.to_json().encode('utf-8'))
 
+def tweet(text):
+    consumer_key        = 'ueIHhQOMuYdQ2jgbWNJ0nllpQ'
+    access_token_key    = '85117657-Lpfqzxr0H6jyk83zm5S2ChfBcpwdmIPvqtOPP1bO0'
+    username            = os.environ['TWITTER_USERNAME']
+
+    with open(PATH_TO_SECRETS) as f:
+        consumer_secret, access_token_secret = map(str.strip, f.read().strip().split("\n"))
+
+    api = twitter.Api(consumer_key=consumer_key,
+                      consumer_secret=consumer_secret,
+                      access_token_key=access_token_key,
+                      access_token_secret=access_token_secret)
+
+    api.PostUpdate(text)
+
 if __name__ == "__main__":
     if not os.path.exists(PATH_TO_MODEL):
         make_model()
@@ -32,4 +49,9 @@ if __name__ == "__main__":
     with open(PATH_TO_MODEL, 'rb') as f:
         model = markovify.NewlineText.from_json(f.read().decode('utf-8'))
 
-    print(model.make_short_sentence(280))
+    text = model.make_short_sentence(280)
+    print(text)
+    tweet(text)
+
+# https://github.com/jsvine/markovify
+# https://github.com/bear/python-twitter
